77d66e23c1650ac922c513591260c0674218f329
Philipp Fehrmann <philipp-09@live.de>
2018-05-13 22:12:28 +0200

    Start here for step 5

--- main.py
+++ main.py
@@ -96,8 +96,70 @@ def get_dataset(dataset='train', shuffle=True, batch_size=250, buffer_size=20000
 # see https://www.tensorflow.org/tutorials/layers
 # see https://github.com/tensorflow/tensorflow/blob/r1.8/tensorflow/examples/tutorials/layers/cnn_mnist.py
 def cnn_model_fn(features, labels, mode):
-    pass
-    # TODO: Define the model here.
+    '''Model function for CNN.'''
+    # Input Layer
+    # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+    input_layer = tf.reshape(features, [-1, image_size_cols, image_size_rows, image_size_channels])
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=16,
+        kernel_size=[5, 5],
+        padding='same',
+        activation=tf.nn.relu)
+    conv2 = tf.layers.conv2d(
+        inputs=conv1,
+        filters=32,
+        kernel_size=[5, 5],
+        padding='same',
+        activation=tf.nn.relu)
+    pool1 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[4, 4], strides=4)
+    conv3 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding='same',
+        activation=tf.nn.relu)
+
+    pool2 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[4, 4], strides=4)
+
+    # TODO: If you change the input size, make sure to adapt these settings. 7*7 is the image size, 64 the number of filters.
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    dense = tf.layers.dense(inputs=dense, units=512, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    # Use 2 units, as we have 2 classes.
+    logits = tf.layers.dense(inputs=dropout, units=2)
+
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        'classes': tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        # optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.002)
+        optimizer = tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        'accuracy': tf.metrics.accuracy(
+            labels=labels, predictions=predictions['classes'])}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 def main(argv):
     args = parser.parse_args(argv[1:])
